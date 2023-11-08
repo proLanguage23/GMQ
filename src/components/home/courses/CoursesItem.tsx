9dd0a9f10db72b7638836081913c7b4786741ad7
mahadidev7 <mahadidev7@gmail.com>
2023-11-08 13:15:41 +0600

    home page updated

--- src/components/home/courses/CoursesItem.tsx
+++ src/components/home/courses/CoursesItem.tsx
@@ -1,37 +1,36 @@
-import { AppDescription, AppSubTitle, PageLink } from "@/components/share";
+import { AppDescription, PageLink } from "@/components/share";
 import Image from "next/image";
 import React from "react";
 
-function CoursesItem({
-  id,
-  img,
-  title,
-  description,
-  btn,
-  class_name = "",
-}: any) {
+function CoursesItem({ img, title, description, btn, class_name = "" }: any) {
   return (
     <div
-      className={`w-full bg-white rounded my-3 md:p-5 p-3 border shadow flex flex-col justify-between flex-wrap ${class_name}`}
+      className={`w-full rounded my-5 flex flex-col justify-between flex-wrap ${class_name}`}
     >
-      <div className="">
-        <Image
-          src={img}
-          alt={"logo"}
-          quality={100}
-          priority={true}
-          className="w-full object-cover rounded h-56"
-        />
-        <PageLink
-          href={btn?.href}
-          text={title}
-          isIcon={false}
-          class_name="mt-3 !justify-start block pb-2 text-[24px] font-medium text-primary capitalize manropeFont hover:underline  transition-all text-secondary"
-        />
-        <AppDescription text={description} class_name="line-clamp-4" />
-      </div>
-      <div className="mt-5 flex md:justify-end">
-        <PageLink {...btn} class_name="commonBtnStyle" />
+      <Image
+        src={img}
+        alt={"logo"}
+        quality={100}
+        width={400}
+        priority={true}
+        className="object-cover h-56 -mb-5 mx-auto rounded-lg"
+      />
+      <div className="bg-secondary md:p-5 p-3 !pb-0 flex-1 flex flex-col justify-between rounded-2xl relative">
+        <div className="df">
+          <PageLink
+            href={btn?.href}
+            text={title}
+            isIcon={false}
+            class_name="mt-3 !justify-start block pb-2 text-[24px] font-medium text-white capitalize manropeFont hover:underline  transition-all text-secondary"
+          />
+          <AppDescription
+            text={description}
+            class_name="line-clamp-4 text-white"
+          />
+        </div>
+        <div className="mt-5 flex -mb-5">
+          <PageLink {...btn} class_name="NewCommonBtnStyle" />
+        </div>
       </div>
     </div>
   );
