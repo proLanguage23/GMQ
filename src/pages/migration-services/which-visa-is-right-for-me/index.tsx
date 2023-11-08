1136ef3416ba7a6c3d4576f5d231b7313c3f2388
mahadidev7 <mahadidev7@gmail.com>
2023-11-08 12:07:03 +0600

    which-visa-is-right-for-me page completed

--- src/pages/migration-services/which-visa-is-right-for-me/index.tsx
+++ src/pages/migration-services/which-visa-is-right-for-me/index.tsx
@@ -1,8 +1,15 @@
 import CommonForm from '@/components/contactForm/CommonForm';
+import WhichVisaIsRightForMeSectionEight from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionEight';
+import WhichVisaIsRightForMeSectionEleven from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionEleven';
 import WhichVisaIsRightForMeSectionFive from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionFive';
 import WhichVisaIsRightForMeSectionFour from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionFour';
+import WhichVisaIsRightForMeSectionNine from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionNine';
+import WhichVisaIsRightForMeSectionSeven from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionSeven';
 import WhichVisaIsRightForMeSectionSix from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionSix';
+import WhichVisaIsRightForMeSectionTen from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionTen';
+import WhichVisaIsRightForMeSectionThirteen from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionThirteen';
 import WhichVisaIsRightForMeSectionThree from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionThree';
+import WhichVisaIsRightForMeSectionTwelve from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionTwelve';
 import WhichVisaIsRightForMeSectionTwo from '@/components/migration-services/which-visa-is-right-for-me/WhichVisaIsRightForMeSectionTwo';
 import { DynamicHead } from '@/components/share';
 import AppBannerImg from '@/components/share/AppBannerImg';
@@ -21,7 +28,13 @@ function WhichVisaIsRightForMePage() {
       <WhichVisaIsRightForMeSectionFour />
       <WhichVisaIsRightForMeSectionFive />
       <WhichVisaIsRightForMeSectionSix />
-      {/* <WhichVisaIsRightForMeSectionSaven /> */}
+      <WhichVisaIsRightForMeSectionSeven />
+      <WhichVisaIsRightForMeSectionEight />
+      <WhichVisaIsRightForMeSectionNine />
+      <WhichVisaIsRightForMeSectionTen />
+      <WhichVisaIsRightForMeSectionEleven />
+      <WhichVisaIsRightForMeSectionTwelve />
+      <WhichVisaIsRightForMeSectionThirteen />
       <CommonForm />
       <TestimonialSection />
     </>
