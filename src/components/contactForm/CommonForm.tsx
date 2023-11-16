dcf076cf62eb37539698b5611392ebc10a416b54
mahadidev7 <mahadidev7@gmail.com>
2023-11-16 16:00:35 +0600

    CommonForm component updated

--- src/components/contactForm/CommonForm.tsx
+++ src/components/contactForm/CommonForm.tsx
@@ -11,7 +11,7 @@ function CommonForm() {
   const { right, left } = CommonFormDummyData;
   return (
     <div className="bg-secondary pt-28">
-      <Container class_name="pb-0">
+      <Container class_name="!pb-0 !mb-0">
         <div className="grid grid-cols-1 md:grid-cols-2 md:gap-9 justify-center items-end">
           <div className="pb-16">
             <AppDescriptionWithDangerouslySetInnerHTML text={left?.form} />
