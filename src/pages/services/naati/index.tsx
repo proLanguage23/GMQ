9c8de5b8319ad523b98317ec6a9ea971f40f5eb9
mahadidev7 <mahadidev7@gmail.com>
2023-11-28 18:51:14 +0600

    bug fix

--- src/pages/services/naati/index.tsx
+++ src/pages/services/naati/index.tsx
@@ -3,14 +3,14 @@ import { DynamicHead, VisitedForm } from "@/components/share";
 import AppSliderBanner from "@/components/share/section/AppSliderBanner";
 import { NaatiSectionOne, NaatiSectionThree, NaatiSectionTwo } from "@/components/student-services";
 import TestimonialSection from "@/components/testimonial";
-import { ProfessionalYearProgramData } from "@/data/BannerData";
+import { ProfessionalYearProgramBannerData } from "@/data/BannerData";
 import React from "react";
 
 function NAATI_PAGE() {
   return (
     <>
       <DynamicHead title="Best PTE Online Courses |  - GMQ Global" />
-      <AppSliderBanner data={ProfessionalYearProgramData} />
+      <AppSliderBanner data={ProfessionalYearProgramBannerData} />
       <NaatiSectionOne />
       <NaatiSectionTwo />
       <NaatiSectionThree />
