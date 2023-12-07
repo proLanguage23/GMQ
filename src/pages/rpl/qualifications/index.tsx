74f904bc7da1b9ebdfe0315b2a5e6ea3fa82a82a
mahadidev7 <mahadidev7@gmail.com>
2023-12-07 11:40:56 +0600

    off of popup elemen v3

--- src/pages/rpl/qualifications/index.tsx
+++ src/pages/rpl/qualifications/index.tsx
@@ -1,14 +1,21 @@
 import RPL_AllQualificationsList from '@/components/rpl/allQualifications/RPL_AllQualificationsList';
 import { AppCourseBanner, DynamicHead, VisitedForm } from '@/components/share'
 import React from 'react'
+import GetYourSkillsRecognizedImg from '@/assets/GetYourSkillsRecognized.png'
 
 function Qualifications() {
+  const popUpData = {
+    type: "image",
+    img : GetYourSkillsRecognizedImg,
+    text: 'Get Your Skills Recognized',
+    href: '/rpl'
+  }
   return (
     <>
       <DynamicHead title="RPL Qualifications-GMQ Global" description="RPL Qualifications-GMQ Global" />
       <AppCourseBanner text={"RPL Qualifications"} />
       <RPL_AllQualificationsList />
-      <VisitedForm />
+      <VisitedForm data={popUpData} />
     </>
   )
 }
