5f5c6eedd2764d38da1c0fb7c19e9c7dac79c452
mahadidev7 <mahadidev7@gmail.com>
2023-12-14 14:46:36 +0600

    testimonial data updated of home page

--- src/data/HomeTestimonialData.tsx
+++ src/data/HomeTestimonialData.tsx
@@ -1,5 +1,14 @@
 import happyStudent from '@/assets/testimonials.png'
-import avatarOne from '@/assets/stedunt-of-testimonials.png'
+import tanverahmad_tm from '@/assets/tanverahmad_tm.png'
+import JaydeepKamani_tm from '@/assets/JaydeepKamani_tm.png'
+import MostafizurRahman_tm from '@/assets/MostafizurRahman_tm.png'
+import DiveshChawla_tm from '@/assets/DiveshChawla_tm.png'
+import ChinMichelle_tm from '@/assets/ChinMichelle_tm.png'
+import souravhussain_tm from '@/assets/souravhussain_tm.png'
+import MinhazurRahmanRafi_tm from '@/assets/MinhazurRahmanRafi_tm.png'
+import Mya_tm from '@/assets/Mya_tm.png'
+import MitulMonpara_tm from '@/assets/MitulMonpara_tm.png'
+import mahadihasan_tm from '@/assets/mahadihasan_tm.png'
 import quotation from '@/assets/quotation.png'
 
 export const HomeTestimonialDummyData = {
@@ -8,26 +17,83 @@ export const HomeTestimonialDummyData = {
     TestimonialDummyData:[
         {
             id: 1,
-            avatar: avatarOne,
-            name: 'tenet max',
-            bio: 'PTE Student',
-            messages:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae onsequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium',
+            avatar: tanverahmad_tm,
+            name: 'tanver ahmad',
+            bio: 'PTE',
+            messages:'GMQ Global provided exceptional guidance, making the education planning process smooth and stress-free. Their expertise and personalized approach exceeded expectations. Highly recommended for anyone seeking a dedicated and knowledgeable consultant. A valuable investment in my educational journey.',
             secondaryImg: quotation
         },
         {
             id: 2,
-            avatar: avatarOne,
-            name: 'max tenet',
-            bio: 'PTE Student',
-            messages:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae onsequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium',
+            avatar: JaydeepKamani_tm,
+            name: 'Jaydeep Kamani',
+            bio: 'RPL',
+            messages:'Highly recommend. Very fantastic experience with my course and got very sound guidance. Thanks team.',
             secondaryImg: quotation
         },
         {
             id: 3,
-            avatar: avatarOne,
-            name: 'jara jik',
-            bio: 'PTE Student',
-            messages:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae onsequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium',
+            avatar: MostafizurRahman_tm,
+            name: 'Mostafizur Rahman',
+            bio: 'NAATI',
+            messages:`A recommendation is extended for GMQ Global to the individuals seeking a smooth and informed transition into different education processes.
+            They show excellent professionalism, knowledge, and personalized approach to provide an efficient and rewarding experience. They have a profound understanding of the intricacies of the application process, providing invaluable insights that streamlined the journey to smooth education process.`,
+            secondaryImg: quotation
+        },
+        {
+            id: 4,
+            avatar: DiveshChawla_tm,
+            name: 'Divesh Chawla',
+            bio: 'Teaching',
+            messages:`Such a wonderful experience. Abu is very knowledgeable and very helpful, he made the whole process very easy. Highly recommended..`,
+            secondaryImg: quotation
+        },
+        {
+            id: 5,
+            avatar: ChinMichelle_tm,
+            name: 'Chin Michelle',
+            bio: 'Hospitality and Cookery',
+            messages:`My friend recommend me the GMQ service. Had  excellent experience. Very good and positive support.`,
+            secondaryImg: quotation
+        },
+        {
+            id: 6,
+            avatar: souravhussain_tm,
+            name: 'sourav hussain',
+            bio: 'Information Technology',
+            messages:`I had an excellent experience with GMQ Global. Their team is highly knowledgeable, reliable, and truly dedicated to helping students achieve their academic goals. From providing valuable insights into various educational programs to offering personalized guidance throughout the application process, GMQ Global demonstrated a level of professionalism and expertise that set them apart. I felt confident in their guidance, and their commitment to transparency and trustworthiness was evident at every step. I highly recommend GMQ Global to anyone seeking reliable and trusted educational consulting services.`,
+            secondaryImg: quotation
+        },
+        {
+            id: 7,
+            avatar: MinhazurRahmanRafi_tm,
+            name: 'Minhazur Rahman Rafi',
+            bio: 'Education Counselling',
+            messages:`I had a remarkable experience with GMQ Global. Their team's profound knowledge, unwavering reliability, and genuine dedication to assisting students in reaching their academic aspirations were truly commendable. From offering valuable insights on diverse educational programs to providing personalized guidance throughout the application process, GMQ Global exhibited a professionalism and expertise that distinguishes them. I had full confidence in their guidance, and their commitment to transparency and trustworthiness shone through every step. I wholeheartedly endorse GMQ Global to anyone in search of dependable and reputable educational consulting services.`,
+            secondaryImg: quotation
+        },
+        {
+            id: 8,
+            avatar: Mya_tm,
+            name: 'Mya',
+            bio: 'Professional Year Program',
+            messages:`Very highly recommend this company especially Abu lovely guy great team to work good luck`,
+            secondaryImg: quotation
+        },
+        {
+            id: 9,
+            avatar: MitulMonpara_tm,
+            name: 'Mitul Monpara',
+            bio: 'PTE',
+            messages:`Highly recommend. Very fantastic experience with my course and got very sound guidance. Thanks team.`,
+            secondaryImg: quotation
+        },
+        {
+            id: 10,
+            avatar: mahadihasan_tm,
+            name: 'mahadi hasan',
+            bio: 'NAATI',
+            messages:`GMQ Global has been an invaluable partner in my educational journey. Their exceptional guidance and personalized approach made the planning process seamless. I highly recommend their expertise for a stress-free and rewarding educational experience.`,
             secondaryImg: quotation
         },
     ]
