585dea5fee38bcfb22b383d73ab092812ae9c0e4
mahadidev7 <mahadidev7@gmail.com>
2024-01-25 16:57:18 +0600

    Meta Pixel Code added second time

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,20 @@ export default function Document() {
       })(window,document,'script','dataLayer','GTM-K3MDJ7ZN');`,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1413997205876287');
+            fbq('track', 'PageView');`,
+          }}
+        />
       </Head>
       <body>
         <Main />
@@ -22,6 +36,14 @@ export default function Document() {
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3MDJ7ZN" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
+        
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1413997205876287&ev=PageView&noscript=1"
+            />`,
           }}
         />
       </body>
